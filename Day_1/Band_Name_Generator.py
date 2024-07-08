1ef00facf3413e53b07b416c252fe62a5d6f3f7e
anonymous <rayappen>
2024-07-08 13:10:08 +0530

    Completed Day 1 Challenge

---
+++ Day_1/Band_Name_Generator.py
@@ -0,0 +1,6 @@
+# Band Name Generator
+# This application helps the user to create a band name based on their city and pet name
+print("Welcome to the Band Name Generator")
+user_city = input("what's the name of the city you grew up in?\n")
+user_pet_name = input("what's your pet's name?\n")
+print("Your band name could be " + user_city + " " + user_pet_name + ".")
---
+++ Day_1/input.py
@@ -0,0 +1,6 @@
+# Testing out the variables
+print("what is your name?")
+user_name = input()
+print("How old are you?")
+user_age = input()
+print("Hello, " + user_name + " and you are " + user_age + " old.")
